d31dbded47f93d338d1f6a3efc6bac48d511ada1
bhaskarj1212 <bhaskarj2003.com>
2021-12-17 15:46:43 +0530

    Contacts page added

--- app/myfolk/src/App.js
+++ app/myfolk/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Contact from './pages/contacts/Contact';
 
 function App() {
   return (
     <div className="App">
-        <Header/>
+        {/* <Header/> */}
+        <Contact/>
     </div>
   );
 }
---
+++ app/myfolk/src/pages/contacts/Contact.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Contact() {
+    return (
+        <>
+        <body>
+            
+      
+        <header>
+         <nav>
+             <ul>
+                 <li><img id='im' src="logo.png" alt="" height={"40px"}/></li>
+                 <li id='contact'><a href="/">Contacts</a></li>
+                  <li><img src="plus1.png" alt="" height={"20px"}/></li>
+                 <li><img src="search.png" alt="" height={"20px"}/></li>
+                 <li><img id='dot' src="dots.png" alt="" height={"20px"}/></li>
+             </ul>
+         </nav>
+        </header>
+        <main>
+
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+        <div className='Contacts'>
+        <img  src="user.png" alt="" height="40px" /><span>Rajesh<br /><span>99546748</span></span><span> <img id='call' src="call.png" alt="" height={"40px"} /></span><span><img src="whatsapp.png" alt="" height={"40px"}/></span><span><img src="dropdown.png" alt="" height={"50px"} /></span>
+         </div>
+
+        <div className="line"></div>
+
+
+
+        </main>
+        <script src="https://kit.fontawesome.com/80ed347dc6.js"
+      crossorigin="anonymous"></script> 
+     </body>
+        </>
+    )
+}
